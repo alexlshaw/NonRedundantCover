cf3d057b7df3ae40ff8c07407cbf918a9ff8fa0d
alexlshaw <alexshaw255@gmail.com>
2024-02-12 16:46:18 +1300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NonRedundantCover
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
